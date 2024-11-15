bc2f3c2ea4e609c2f844da5f0df49f0e7521b01c
Sakshi <sakshi031997@gmail.com>
2024-11-15 10:53:40 -0500

    fixed flake8 and added print statements

--- load-quality.py
+++ load-quality.py
@@ -1,7 +1,6 @@
 import psycopg
 import pandas as pd
 import sys
-import numpy as np
 import credentials
 from datetime import datetime
 from psycopg import errors
@@ -11,7 +10,7 @@ import helper_functions as hf
 
 def check_and_update_static_data(conn, data, columns):
     """
-    Checks for discrepancies between HospitalsQualityDetails and 
+    Checks for discrepancies between HospitalsQualityDetails and
     HospitalSpecificDetails and updates them.
 
     Parameters:
@@ -23,13 +22,14 @@ def check_and_update_static_data(conn, data, columns):
     - The function performs the following tasks:
       - Reads existing data from the `HospitalSpecificDetails` table based on
         the `hospital_pk` values in the batch.
-      - Identifies discrepancies between the batch data and `HospitalSpecificDetails` by performing a join 
-        and checking for mismatched columns.
-      - Constructs a DataFrame of discrepancies (`discrepancies_df`) containing records where column values 
-        do not match the existing data in `HospitalSpecificDetails`.
-      - Executes an update query to modify mismatched rows in `HospitalSpecificDetails` based on `hospital_pk`.
+      - Identifies discrepancies between the batch data and
+        `HospitalSpecificDetails` by performing a join and mismatched columns.
+      - Constructs a DataFrame of discrepancies (discrepancies_df) with
+        records that do not match the existing data in HospitalSpecificDetails
+      - Executes an update query to modify mismatched rows in
+        `HospitalSpecificDetails` based on `hospital_pk`.
     """
-    
+
     data = data.copy()
     # read HospitalSpecificDetails for hospital_pk in qualtiy data batch
     with conn.transaction():
@@ -37,33 +37,34 @@ def check_and_update_static_data(conn, data, columns):
 
         # Execute the query with the hospital_pks tuple as a parameter
         cur = conn.cursor()
-        cur.execute("SELECT hospital_pk, hospital_name, address, city, zip, state "
+        cur.execute("SELECT hospital_pk,hospital_name,address,city,zip,state "
                     "FROM HospitalSpecificDetails "
                     "WHERE hospital_pk IN (" + params + ")")
 
         static_data = cur.fetchall()
         h_df = pd.DataFrame(static_data, columns=columns)
 
-        # join h_df with the batch data, and see which rows are not present in h_df
+        # join h_df with the batch data, and see which rows are not in h_df
         data = data[columns]
         data = data[data['hospital_pk'].isin(list(h_df['hospital_pk']))]
-        h_df = h_df.merge(data, on = columns, how = "right" , indicator=True)
-        # whereall the static values does not exactly match, indicator will say 'right_only'
-        
-        discrepencies_df = h_df[h_df['_merge']=='right_only'].drop(columns=['_merge'])
-        # fix the order of columns to be inline with update query
-        col_order = [x for x in columns if x not in ['hospital_pk']] + ['hospital_pk']
+        h_df = h_df.merge(data, on=columns, how="right", indicator=True)
+        # if the static values does not match, indicator will be 'right_only'
 
-        # Before inserting values into HospitalQualityDetails, update 
+        discrepencies_df = h_df[h_df['_merge'] == 'right_only'].\
+            drop(columns=['_merge'])
+        # fix the order of columns to be inline with update query
+        col_order = [x for x in columns if x not in ['hospital_pk']] \
+            + ['hospital_pk']
+
+        # Before inserting values into HospitalQualityDetails, update
         # the values for these discrepencies
         update_values = [
             (tuple(row[col] for col in col_order))
             for idx, row in discrepencies_df.iterrows()
         ]
-        print(update_values)
         cur.executemany(queries.STATIC_DETAILS_UPDATE_QUERY, update_values)
         print("Updation Successful for HospitalSpecificData")
-            
+
 
 def batch_insert_cms_data(conn, data, batch_size=100):
     """
@@ -78,14 +79,15 @@ def batch_insert_cms_data(conn, data, batch_size=100):
     Notes:
     - The function performs the following transformations:
       - Defines SQL insertion queries for insertion.
-      - If any hospital details in the quality data differ from those stored in 
+      - If any hospital details in the quality data differ from those stored in
         HospitalSpecificDetails, the function updates them
       - Inserts rows in 'HospitalQualityDetails' table in batches.
-      - Handles ForeignKeyViolation error by first inserting into 'HospitalSpecificDetails'
-        if required, and then retries insertion into 'HospitalQualityDetails'.
+      - Handles ForeignKeyViolation error by first inserting into
+        'HospitalSpecificDetails' if required, and then retries insertion into
+        'HospitalQualityDetails'.
       - Uses 'ON CONFLICT DO NOTHING' to prevent duplicate entries on conflict.
     """
-    
+
     cur = conn.cursor()
 
     quality_data_cols = [
@@ -109,20 +111,23 @@ def batch_insert_cms_data(conn, data, batch_size=100):
     for row_index in range(0, len(data), batch_size):
         batch_df = data[row_index:row_index + batch_size]
         print("Running process for batch", (row_index // batch_size) + 1)
-        
-        # Check if hospital-specific column in quality data matches 
+        print("Number of rows in batch ", (row_index // batch_size) + 1,
+              batch_size)
+
+        # Check if hospital-specific column in quality data matches
         # HospitalSpecificDetails, update if not
         check_and_update_static_data(conn, batch_df, static_data_cols)
-            
+
         # Prepare values for insertion in HospitalQualityDetails
         quality_values = [
             (tuple(row[col] for col in quality_data_cols))
             for idx, row in batch_df.iterrows()
         ]
-            
+
         try:
             with conn.transaction():
-                cur.executemany(queries.HOSPITAL_QUALTIY_DETAILS_INSERT_QUERY, quality_values)
+                cur.executemany(queries.HOSPITAL_QUALTIY_DETAILS_INSERT_QUERY,
+                                quality_values)
                 print("Insertion successful for HospitalQualityDetails")
         except errors.ForeignKeyViolation:
             # Handle foreign key violation by inserting
@@ -135,15 +140,17 @@ def batch_insert_cms_data(conn, data, batch_size=100):
                 (tuple(row[col] for col in static_data_cols))
                 for idx, row in batch_df.iterrows()
             ]
-            
+
             # Insert into HospitalSpecificDetails to resolve FK dependency
             with conn.transaction():
-                cur.executemany(queries.STATIC_DETAILS_INSERT_QUERY, static_values)
+                cur.executemany(queries.STATIC_DETAILS_INSERT_QUERY,
+                                static_values)
                 print("Insertion successful for HospitalSpecificDetails")
 
             # Reinserting into HospitalQualityDetails after resolving FK error
             with conn.transaction():
-                cur.executemany(queries.HOSPITAL_QUALTIY_DETAILS_INSERT_QUERY, quality_values)
+                cur.executemany(queries.HOSPITAL_QUALTIY_DETAILS_INSERT_QUERY,
+                                quality_values)
                 print("Insertion successful for HospitalQualityDetails")
 
         except Exception as e:
@@ -159,10 +166,11 @@ if __name__ == "__main__":
     file_path = sys.argv[2]
     last_updated = datetime.strptime(sys.argv[1], "%Y-%m-%d").date()
 
-    data = pd.read_csv(file_path)   
+    data = pd.read_csv(file_path)
+    print("Data has ", len(data), " rows in total")
     # insert last_updated column in the data. We get it from sys.args
     data['last_updated'] = last_updated
-    
+
     processed_data = hf.process_cms_data(data)
 
     conn = psycopg.connect(
